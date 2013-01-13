b0bb3d7fb313dc2d8c3fb275eff9e84d3efa3702
Adam <orion186@gmail.com>
2013-01-12 22:13:36 -0800

    Update gerbal.sh
    Spelling errors, backpurp to deepblue on G4, eliminated line 63,77, added line 106,

--- gerbal.sh
+++ gerbal.sh
@@ -13,16 +13,16 @@ function g2() {
         MESSAGE=${@:-"${RESET}ALL DONE!!"}
         echo -e "${COLOR}${MESSAGE}${RESET}"
 }
-# Distplays colorized wrning output
+# Distplays colorized warning output
 function g3() {
         COLOR='\e[1;32m'        #Bold Green
         RESET='\033[00;00m'     # normal white
         MESSAGE=${@:-"${RESET}ALL DONE!!"}
         echo -e "${COLOR}${MESSAGE}${RESET}"
 }
-# Distplays colorized wrning output
+# Distplays colorized warning output
 function g4() {
-        COLOR='\e[45m'          #backpur
+        COLOR='\e[1;32m"'          #Bold Blue
         RESET='\033[00;00m'     #normal white
         MESSAGE=${@:-"${RESET}ALL DONE!!"}
         echo -e "${COLOR}${MESSAGE}${RESET}"
@@ -50,34 +50,32 @@ function ConfirmOrExit() {
 
 
 #welcome message
-g3 "Welcome to Gerbal!, a script writen to make YOUR life easier"
+g3 "Welcome to Gerbal!, a script writen to make YOUR life easier."
 sleep 3
-g3 "This script will help you to do task on the server with some automation"
+g3 "This script will help you to do task on the server with some automation."
 sleep 3
 g3 "Keep in mind it only does what you TELL, it to do ;) "
 ConfirmOrExit
-#start with checking uptime
+#start with checking uptime!
 sleep 3
 g4 "Checking server uptime..."
 sleep 1
 uptime
 sleep 1
-g3 "should the Gerbal do more? or is this all you needed?"
 ConfirmOrExit
 
 g4 "Checking free memory..."
 sleep 1
 free -t -m
 ConfirmOrExit
+
 sleep 2
 g4 "Checking Memory Usage"
 sleep 1
 egrep -h --color 'Mem|Cache|Swap' /proc/meminfo
-
 sleep 3
 g3 "all done with memory checks!"
 sleep 2
-g3 "should the Gerbal do more? or is this all you needed?"
 ConfirmOrExit
 
 g4 "Checking server disk space"
@@ -103,8 +101,9 @@ sleep 2
 g1 "Disk useage on andrewph"
 du -sh /home/andrewph
 sleep 2
-g4 "disk useage print done"
+g4 "disk useage print out done"
 sleep 1
+g4 "read your profile's Disk useage printout, if it seems overly large, please clean up your home dir ;) thx!"
 ConfirmOrExit
 
 sleep 2
@@ -112,6 +111,8 @@ g4 "checking what ports are open on server!"
 sleep 1
 netstat -tulpn
 sleep 3
+g4 "watch for the Minecraft ports (25565, 25566, ect) and ssh ports (22)."
+sleep 2
 ConfirmOrExit
 
 sleep 2
@@ -129,19 +130,19 @@ ConfirmOrExit
 
 g4 "OK now for the fun stuff!!"
 sleep 2
-g4 "running updates to server deb packages"
+g3 "running updates to server deb packages"
 sleep 2
 apt-get update
 sleep 3
-g4 "running upgrades to server systems from deb packages"
+g3 "running upgrades to server systems from deb packages"
 sleep 2
 apt-get upgrade -y
 sleep 3
-g4 "running aptitude for checksum"
+g3 "running aptitude for checksum"
 sleep 2
 aptitude update
 sleep 3
-g4 "updating packages on aptitued recomendation"
+g3 "updating packages on aptitued recomendation"
 aptitude upgrade -y
 sleep 4
 ConfirmOrExit
