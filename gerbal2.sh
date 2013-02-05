2a07d94cf3f6d20d033f3c47ecf7f418dab13cd2
monkeypuddles <monkeyservers@gmail.com>
2013-02-06 02:02:49 +0400

    new versioning, new ideas

---
+++ gerbal2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Global Varibles
+TEMPDIR="/tmp/"
+DATE_FORMAT="%Y%m%d"
+WEEK=`date +%V`
+CMDLIST="`date -u "+"$DATE_FORMAT" @%H%MUTC"` W$WEEK =>todo cmd1 cmd2 ?>"
+VERSION="v0.9.4.20090306"
+
+##########################################################
+
+# Lists and such
+
+
+#main Menu
+
+menu ()
+
+{
+tput clear
+
+echo "Gerbal 2 menu"
+echo "
+
+==========================================================================================================
+
+	com1		somecommand explanation
+	com2		somecommand explanation
+	todo		list out todos
+
+==========================================================================================================
+"
+
+grep "# cmdname=" /usr/local/bin/gerbal2 -A2
+}
+
+todo ()
+{
+echo "
+	this is basic to do area
+	can list out any info needed
+
+"
+
+}
+
+
+	
+
